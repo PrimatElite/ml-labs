aff64174763932d81e1954f8d9343d30cfc0af8e
LanskovNV <leins275@gmail.com>
2021-10-20 14:25:32 +0300

    #4 Updated error handling

--- src/util/__init__.py
+++ src/util/__init__.py
@@ -7,31 +7,32 @@ import yaml
 def load_config(path: str) -> dict:
     try:
         with open(path, "r") as stream:
-            try:
-                document = yaml.safe_load(stream)
-            except yaml.YAMLError as e:
-                print("Yaml error:", e)
+            document = yaml.safe_load(stream)
     except OSError as e:
         print("Read config error:", e)
+        return {}
+    except yaml.YAMLError as e:
+        print("Yaml error:", e)
+        return {}
 
     return document
 
 
 def validate_config(obj: dict) -> bool:
     scheme_path = "./restrictions_schema.json"
+
     try:
         with open(scheme_path, "r") as file:
             schema = json.load(file)
+        validate(instance=obj, schema=schema)
     except OSError as e:
         print("Open file error:", e)
         return False
     except json.decoder.JSONDecodeError as e:
         print("Parse schema error:", e)
         return False
-
-    try:
-        validate(instance=obj, schema=schema)
     except jsonschema.exceptions.ValidationError as err:
         print("Validation error:", err)
         return False
+
     return True
