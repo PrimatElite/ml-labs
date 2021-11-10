36026912e687c99bf173780c82c8843783460395
LanskovNV <leins275@gmail.com>
2021-11-10 14:34:44 +0300

    #4 Fixed codestyle in test_util.py

--- src/util/test_util.py
+++ src/util/test_util.py
@@ -5,13 +5,13 @@ import pytest
 from . import load_config, validate_config
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def resource_setup():
-    print('\n Init')
-    examples_path = './config_examples'
+    print("\n Init")
+    examples_path = "./config_examples"
     examples = next(os.walk(examples_path), (None, None, []))[2]
 
-    return [{'name': ex, 'config': load_config(os.path.join(examples_path, ex))} for ex in examples]
+    return [{"name": ex, "config": load_config(os.path.join(examples_path, ex))} for ex in examples]
 
 
 def test_validate_examples(resource_setup):
@@ -21,8 +21,8 @@ def test_validate_examples(resource_setup):
 
 
 def validate(obj):
-    print('\n', obj['name'])
-    if obj['name'].split('_')[0] == 'good':
-        assert validate_config(obj['config'])
+    print("\n", obj["name"])
+    if obj["name"].split("_")[0] == "good":
+        assert validate_config(obj["config"])
     else:
-        assert not validate_config(obj['config'])
+        assert not validate_config(obj["config"])
